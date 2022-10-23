f19a7a72c459a20f8ceccd753874acecf034fe0c
Eric Rawn <erawn@berkeley.edu>
2022-10-22 20:19:39 -0700

    express mutual TLS

--- express/index.tsx
+++ express/index.tsx
@@ -5,25 +5,35 @@ import https from "https";
 import fs from "fs";
 
 dotenv.config();
-const app: Express = express();
+const app : Express = express();
 app.use(helmet());
 const port = process.env.PORT;
 //https://medium.com/@sevcsik/authentication-using-https-client-certificates-3c9d270e8326
 //https://www.sitepoint.com/how-to-use-ssltls-with-node-js/
+
+interface RequestWithClient extends Request {
+  client?: any
+}
+
+app.get('/', (req:RequestWithClient, res:Response) => {
+  if (!req.client.authorized) {
+    return res.status(401).send('Invalid client certificate authentication.');
+  }
+
+  return res.send('Hello, world!');
+});
+
 https
   .createServer( 
     {
-    key: fs.readFileSync("key.pem"),
-    cert: fs.readFileSync("cert.pem"),
+    requestCert: true,
+    rejectUnauthorized: false,
+    ca: fs.readFileSync('root.crt'),
+    key: fs.readFileSync("server.key"),
+    cert: fs.readFileSync("server.crt"),
    },
     app
   )
-  .listen(4000, ()=>{
+  .listen(port, ()=>{
     console.log('server is runing at port 4000')
   });
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});

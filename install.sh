8554c7444eda81e685157756a72b3a3e75043041
Eric Rawn <erawn65@gmail.com>
2023-03-16 19:28:47 -0700

    yes

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ sudo apt-get install iptables
 
 yes | curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
 
-echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+yes | echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
 
 sudo apt-get update
 
